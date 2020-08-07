1cedd2da03146b018859699c999f3ac119019b9d
bamblebam <burhanizrangwala@gmail.com>
2020-08-07 12:20:00 +0530

    1

--- content/views.py
+++ content/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+import requests
 # Create your views here.
 
 
 class Home(View):
     template_name = 'content/home.html'
+    url = 'https://api.github.com/users/{}'
 
     def get(self, request):
+        data = requests.get(self.url.format('bamblebam')).json()
+        print(data)
         return render(request, self.template_name)
 
 # def Home(request):
