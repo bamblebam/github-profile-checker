891e1c53ee8dd24beb66fd44413e6a3af39016af
bamblebam <burhanizrangwala@gmail.com>
2020-08-09 01:27:29 +0530

    lang thing

--- content/views.py
+++ content/views.py
@@ -10,10 +10,22 @@ class Home(View):
 
     def get(self, request):
         data = requests.get(self.url.format('bamblebam')).json()
+
         follower_url = data['followers_url']
         follower_data = requests.get(follower_url).json()
         follower_list = [follower for follower in follower_data]
-        print(follower_list[0])
+
+        repos_url = data['repos_url']
+        repo_data = requests.get(repos_url).json()
+        repo_list = [repo for repo in repo_data]
+        language_list = [repo["language"]
+                         for repo in repo_list if repo["language"]]
+        language_set = list(set(language_list))
+        language_dict = dict()
+        for language in language_set:
+            count = language_list.count(language)
+            language_dict[language] = count
+
         context = {
             'data': data,
             'follower_list': follower_list
